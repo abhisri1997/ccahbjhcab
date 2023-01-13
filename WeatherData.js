dd97f8d1d8cc98af8af31c936c7e07baa2dc711e
Abhinav Kumar <abhinav.kumar@solitontech.com>
2023-01-13 05:59:04 +0530

    docs: Documentation added

--- WeatherData.js
+++ WeatherData.js
@@ -1,3 +1,8 @@
+/**
+ * Returns weather information in json format
+ *
+ * @return {json} JSON representation
+ */
 const getWeatherData = async () => {
   const response = await fetch("./assets/data/data.json");
   const data = await response.json();
