9c9550b5a6e2a989d58604e29c2242d8184b0a3e
Abhinav Kumar <abhinav.kumar@solitontech.com>
2023-01-31 17:45:56 +0530

    feat:js: add class inheritance

--- js/Cities.js
+++ js/Cities.js
@@ -1,29 +1,33 @@
 import City from "./City.js";
 import getWeatherData from "./WeatherData.js";
 
-/**
- * Takes arrays of City objects
- *
- * @param {Array<City>} array of city objects
- */
-function Cities(cities) {
-  City.call(this);
-  this.cities = cities ? cities : [];
+class Cities extends City {
+  constructor(cities) {
+    super();
+    this.cities = cities ? cities : [];
+    this.cityNextFiveHrsForecast = "";
+  }
+  getCity(cityValue) {
+    return this.cities.find((city) => city.cityValue === cityValue);
+  }
+  addCity(city) {
+    this.cities.push(city);
+  }
+  removeCity(cityName) {
+    this.cities = this.cities.filter((city) => city.cityName !== cityName);
+  }
+  setCityForecast(cityValue) {
+    const data = getWeatherData();
+    const cityObj = data[cityValue];
+    this.cityNextFiveHrsForecast = cityObj.nextFiveHrs;
+  }
+  getCityForecast(cityValue) {
+    if (!this.cityNextFiveHrsForecast) {
+      const city = this.cities.find((city) => city.cityValue === cityValue);
+      this.setCityForecast(city.cityValue);
+    }
+    return this.cityNextFiveHrsForecast;
+  }
 }
-
-Cities.prototype = Object.create(City.prototype);
-Cities.prototype.constructor = Cities;
-
-Cities.prototype.getCity = function (cityValue) {
-  return this.cities.find((city) => city.cityValue === cityValue);
-};
-
-Cities.prototype.addCity = function (city) {
-  this.cities.push(city);
-};
-
-Cities.prototype.removeCity = function (cityName) {
-  this.cities = this.cities.filter((city) => city.cityName !== cityName);
-};
 
 export default Cities;
--- js/City.js
+++ js/City.js
@@ -1,111 +1,48 @@
 import { getCityDate, getCityTime, getCitySession } from "./CityDateAndTime.js";
 import getWeatherData from "./WeatherData.js";
 
-/**
- * City constructor function takes city information as parameters
- *
- * @param {string} cityValue
- * @param {string} cityName - City name
- * @param {string} cityHumidity - City humidity
- * @param {string} cityTemp - City temperature
- * @param {string} cityPrecipitation - City Precipitation
- * @param {string} cityContinent - City Continent
- * @param {string} cityDateAndTime - City Date and Time
- */
-function City(
-  cityValue,
-  cityName,
-  cityHumidity,
-  cityTemp,
-  cityPrecipitation,
-  cityContinent,
-  cityDateAndTime
-) {
-  this.cityValue = cityValue;
-  this.cityName = cityName;
-  this.cityTemp = cityTemp;
-  this.cityHumidity = cityHumidity;
-  this.cityPrecipitation = cityPrecipitation;
-  this.cityContinent = cityContinent;
-  this.cityDateAndTime = cityDateAndTime;
-  this.cityNextFiveHrsForecast = "";
-}
-
-/**
- *
- * @returns {string} City Name
- */
-City.prototype.getCityName = function () {
-  return this.cityName;
-};
-
-/**
- * Return city date
- * @returns {string} - City Date
- */
-City.prototype.getCityDate = function () {
-  return getCityDate(this.cityDateAndTime);
-};
-
-/**
- * Return city time
- * @returns {string} - City Time
- */
-City.prototype.getCityTime = function () {
-  return getCityTime(this.cityDateAndTime);
-};
-
-/**
- * Returns city session
- * @returns {string} - City Session
- */
-City.prototype.getCitySession = function () {
-  return getCitySession(this.cityDateAndTime);
-};
-
-/**
- * Return city continent
- * @returns {string} - City Continent
- */
-City.prototype.getCityTimeZone = function () {
-  return this.cityContinent ? this.cityContinent.split("/")[0] : "";
-};
-
-/**
- * Returns city temperature
- * @returns {Number} - City Temperature
- */
-City.prototype.getCityTemperature = function () {
-  return this.cityTemp ? parseInt(this.cityTemp) : "";
-};
-
-/**
- * Returns city humidity
- * @returns {Number} - City Humidity
- */
-City.prototype.getCityHumidity = function () {
-  return this.cityHumidity ? parseInt(this.cityHumidity) : "";
-};
-
-/**
- * Returns city precipitation
- * @returns {Number} - City Precipitation
- */
-City.prototype.getCityPrecipitation = function () {
-  return this.cityPrecipitation ? parseInt(this.cityPrecipitation) : "";
-};
-
-City.prototype.setCityForecast = function (cityValue) {
-  const data = getWeatherData();
-  const cityObj = data[cityValue];
-  this.cityNextFiveHrsForecast = cityObj.nextFiveHrs;
-};
-
-City.prototype.getCityForecast = function (cityValue) {
-  if (!this.cityNextFiveHrsForecast) {
-    this.setCityForecast(cityValue);
+class City {
+  constructor(
+    cityValue,
+    cityName,
+    cityHumidity,
+    cityTemp,
+    cityPrecipitation,
+    cityContinent,
+    cityDateAndTime
+  ) {
+    this.cityValue = cityValue;
+    this.cityName = cityName;
+    this.cityTemp = cityTemp;
+    this.cityHumidity = cityHumidity;
+    this.cityPrecipitation = cityPrecipitation;
+    this.cityContinent = cityContinent;
+    this.cityDateAndTime = cityDateAndTime;
   }
-  return this.cityNextFiveHrsForecast;
-};
+  getCityName() {
+    return this.cityName;
+  }
+  getCityDate() {
+    return getCityDate(this.cityDateAndTime);
+  }
+  getCityTime() {
+    return getCityTime(this.cityDateAndTime);
+  }
+  getCitySession() {
+    return getCitySession(this.cityDateAndTime);
+  }
+  getCityTimeZone() {
+    return this.cityContinent ? this.cityContinent.split("/")[0] : "";
+  }
+  getCityTemperature() {
+    return this.cityTemp ? parseInt(this.cityTemp) : "";
+  }
+  getCityHumidity() {
+    return this.cityHumidity ? parseInt(this.cityHumidity) : "";
+  }
+  getCityPrecipitation() {
+    return this.cityPrecipitation ? parseInt(this.cityPrecipitation) : "";
+  }
+}
 
 export default City;
