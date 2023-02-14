036af12cc247d31f318e13323ba32fad3435b522
Abhinav Kumar <abhinav.kumar@solitontech.com>
2023-02-14 21:11:03 +0530

    feat:nodejs: add deployment support

--- js/WeatherData.js
+++ js/WeatherData.js
@@ -6,7 +6,8 @@
  * @return {json} JSON representation
  */
 const getWeatherData = async () => {
-  const apiResponse = await fetch("http://127.0.0.1:3000/all-timezone-cities");
+  const base = `${location.protocol}//${location.host}`;
+  const apiResponse = await fetch(`${base}/all-timezone-cities`);
   const data = await apiResponse.json();
   return data;
 };
